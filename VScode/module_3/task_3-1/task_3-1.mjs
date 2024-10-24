2a2d899f7a7032805526da23837a8278d617d570
solvar16 <solvar.sun@gmail.com>
2024-10-24 13:49:38 +0200

    task 3.1 complete

--- VScode/module_3/task_3-1/task_3-1.mjs
+++ VScode/module_3/task_3-1/task_3-1.mjs
@@ -12,43 +12,173 @@ Print out to the HTML page the expression statement you made
 
 printOut("Task 1, 2 and 3");
 
-let wakeUpTime = 2;
-const wakeUpTime1 = 6;
-const wakeUpTime2 = 7;
-const wakeUpTime3 = 8;
+let wakeUpTime = 8;
+const haveToWakeUp = 7;
+const wakeForTrain = 8;
+printOut("I woke up at " + wakeUpTime);
 
-if (wakeUpTime > wakeUpTime2) {
-  printOut("I can take the bus to school.");
+if (wakeUpTime <= haveToWakeUp) {
+  printOut("I can take the bus to school because i woke up by " + haveToWakeUp);
 }
+else if (wakeUpTime == wakeForTrain){
+  printOut("I woke up at " + wakeUpTime + " so i'm too late for the bus, but can catch the train to school.");
+}
+else {
+  printOut("I have to take the car to school.");
+}
+
+
 printOut(newLine);
 
+
 printOut("--- Part 4, 5 --------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Write an if statement that checks whether an integer variable is negative or positive, print the text
+"Positive" or "Negative" accordingly. Run the program with different types of values for the variable to
+check the if statement
+
+Change part 4 to print “Positive”, “Negative” or “Zero” accordingly. Run the program with different types of
+values for the variable to check the "if" statement.
+*/
+const myNumber = 0;
+printOut("My number is: " + myNumber);
+
+if (myNumber > 0){
+  printOut("The number is positive.");
+}
+else if (myNumber < 0){
+  printOut("The number is negative");
+}
+else {
+  printOut("The number is zero");
+}
+
+
 printOut(newLine);
 
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Imagine you have a photo editing profession. And you have a website where people can upload pictures
+for you to work on. However, the images must be 4MP or larger, if they are smaller, you cannot use them.
+Create a variable that holds a generated random integer between 1 and 8 (inclusive). Use this variable to
+simulate the uploaded image size and print it.
+Then create an if statement that prints out “Thank you” if the
+size is equal to or greater than the limit. Otherwise, print out "The image is too small"
+*/
+let imageMP = Math.floor(Math.random()*8)+ 1; //+ 1 for at det genererte nummueret skal starte på 1. Da kommer også tall 8 med i genereringen.
+printOut("The uploaded image size is " + imageMP + "MP");
+
+if(imageMP >= 4){
+  printOut("Thank you!");
+}
+else{
+  printOut("The image is too small!");
+}
+
 printOut(newLine);
 
+
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Expand part 6 to exclude if the image size is larger or equal to 6MP, then print out “Image is too large”
+*/
+let image = Math.floor(Math.random()*8)+ 1; 
+printOut("The uploaded image size is " + image + "MP");
+
+if(image >= 4 && image < 6){
+  printOut("Thank you!");
+}
+else if(image >= 6){
+  printOut("The image is too large!")
+}
+else {
+  printOut("The image is too small!");
+}
+
 printOut(newLine);
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Print if monthName contains “r”: “You must take vitamin D” else “You do not need to take vitamin D”
+*/
+
+const monthList =["January", "February", "March", "April", "May",
+  "June", "July", "August", "September", "October", "November", "December"];
+  const noOfMonth = monthList.length;
+  const monthName = monthList[Math.floor(Math.random() * noOfMonth)];
+  printOut("Month: " + monthName)
+
+if (monthName.includes("r")){
+  printOut("You must take vitamin D.");
+}
+else {
+  printOut("You do not need to take vitamin D");
+}
+
+let daysOfMonth = null
+switch (monthName){
+  case "January":
+  case "March":
+  case "May":
+  case "July":
+  case "August":
+  case "October":
+  case "December":
+  daysOfMonth = 31
+break;
+  case "April":
+  case "June":
+  case "September":
+  case "November":
+  daysOfMonth = 30
+break;
+  case "February":
+  daysOfMonth = 28
+}
+
+printOut("There are " + daysOfMonth + " days in " + monthName);
+
+
 printOut(newLine);
 
 printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
+/* Expand exercise 8 to print how many days there are in the current month. And do not use date object.
+*/
+
+
 printOut("Replace this with you answer!");
 printOut(newLine);
 
 /* Task 10*/
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Imagine you have an art gallery, but you need to refurbish the premises, so you close the gallery from
+March through May, but in April you have temporary premises in the building next door. Use the month
+constant in exercise 8 to inform the status of your gallery in that month.
+*/
+
+const monthList2 =["January", "February", "March", "April", "May",
+  "June", "July", "August", "September", "October", "November", "December"];
+  const noOfMonth2 = monthList2.length;
+  const monthName2 = monthList2[Math.floor(Math.random() * noOfMonth2)];
+  printOut("Month: " + monthName2)
+
+let galleryStatus = null
+switch (monthName2){
+  case "January":
+  case "February":
+  case "June":
+  case "July":
+  case "August":
+  case "September":
+  case "October":
+  case "November":
+  case "December":
+  galleryStatus = "Open"
+break
+  case "April":
+  galleryStatus = "Open next door"
+break;
+  case "March":
+  case "May":
+  galleryStatus = "closed"
+}
+
+printOut("Gallery Status: " + galleryStatus);
+
 printOut(newLine);
--- VScode/module_3/task_3-2/task_3-2.mjs
+++ VScode/module_3/task_3-2/task_3-2.mjs
@@ -4,17 +4,46 @@ initPrintOut(document.getElementById("txtOut"));
 
 printOut("--- Part 1 ----------------------------------------------------------------------------------------------");
 /* Put your code below here!*/
+
+
+
 printOut("Replace this with you answer!");
 printOut(newLine);
 
 printOut("--- Part 2 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Create a program that can guess a number between 1 and 60. Declare a variable and assign it a value, for
+example, 45. Let the computer "guess" by generating a random number. Use a "while" loop and the
+"random" function. Keep the "while" loop running as long as the "guessed number" is incorrect. Print the
+number once the "while" loop has completed. You do not need to print anything while the "while" loop is in
+progress
+*/
+
+const answerNumber = 45;
+let guessNumber = 0;
+while(answerNumber !== guessNumber){
+    guessNumber = Math.ceil(Math.random()* 60); //ceil genererer fra fra 1 til 60, mens math.floor genererer fra 0 til 60
+}
+printOut("Guess Number = " + guessNumber.toString());
+
 printOut(newLine);
 
 printOut("--- Part 3 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Take the program from part 2 and expand it to guess a number between 1 and one million. Print the
+number of guesses as well as the number of milliseconds it took to guess the number. HINT: Use the
+Date.now() function to measure time
+*/
+
+guessNumber = 0;
+let guessCount = 0;
+const startTime = Date.now();
+while(answerNumber !== guessNumber){
+    guessCount++;
+    guessNumber = Math.ceil(Math.random() * 100000);
+}
+const endTime = Date.now();
+const timeUsed = endTime - startTime;
+printOut("Antall gjetninger " + guessCount.toString() + " tok " + timeUsed.toString() + " ms");
+
 printOut(newLine);
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
@@ -33,8 +62,70 @@ printOut("Replace this with you answer!");
 printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Simulate 6 dice and print how many "throws" it takes to get:
+● 1 2 3 4 5 6 (full straight)
+● 3 pairs
+● 2 of a kind and 4 of a kind (tower)
+● All the same (Yahtzee)
+*/
+
+printOut("Dice rolling");
+
+const d1 = Math.ceil(Math.random() * 6);
+const d2 = Math.ceil(Math.random() * 6);
+const d3 = Math.ceil(Math.random() * 6);
+const d4 = Math.ceil(Math.random() * 6);
+const d5 = Math.ceil(Math.random() * 6);
+const d6 = Math.ceil(Math.random() * 6);
+
+let diceThrow = "";
+diceThrow += d1.toString() + ",";
+diceThrow += d2.toString() + ",";
+diceThrow += d3.toString() + ",";
+diceThrow += d4.toString() + ",";
+diceThrow += d5.toString() + ",";
+diceThrow += d6.toString();
+
+diceThrow = "6,6,3,3,5,5";
+
+printOut("diceThrow: " + diceThrow);
+
+const count1 = (diceThrow.match(/1/g) || "").length;
+const count2 = (diceThrow.match(/2/g) || "").length;
+const count3 = (diceThrow.match(/3/g) || "").length;
+const count4 = (diceThrow.match(/4/g) || "").length;
+const count5 = (diceThrow.match(/5/g) || "").length;
+const count6 = (diceThrow.match(/6/g) || "").length;
+
+let diceCount = "";
+diceCount += count1.toString() + ",";
+diceCount += count2.toString() + ",";
+diceCount += count3.toString() + ",";
+diceCount += count4.toString() + ",";
+diceCount += count5.toString() + ",";
+diceCount += count6.toString() + ",";
+printOut("diceCount: " + diceCount);
+
+const equals1 = (diceCount.match(/1/g) || "").length;
+const equals2 = (diceCount.match(/2/g) || "").length;
+const equals4 = (diceCount.match(/4/g) || "").length;
+const equals6 = (diceCount.match(/6/g) || "").length;
+printOut("equals1: " + equals1.toString());
+printOut("equals2: " + equals2.toString());
+printOut("equals4: " + equals4.toString());
+printOut("equals6: " + equals6.toString());
+
+
+if(equals1 === 6){
+    printOut("Full straight");
+}else if (equals6 === 1){
+    printOut("Yatzy");
+}else if(equals2 === 1 && equals4 === 1){
+    printOut("Tower");
+}else if(equals2 === 3){
+    printOut("3 par");
+}
+
 printOut(newLine);
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
